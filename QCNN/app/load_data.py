a64a4532dec47d08229411356bd49b91917f8b5d
Hye-yeon-5 <1244186201@qq.com>
2025-01-15 22:53:07 +0800

    Add files via upload

--- QCNN/app/load_data.py
+++ QCNN/app/load_data.py
@@ -1,21 +1,15 @@
-import os
 from torch.utils.data import Dataset
-import sys
 import numpy as np
 import torch
-from sklearn.preprocessing import normalize
 
 
 class MyCSVDatasetReader(Dataset):
 
     def __init__(self, csv_path):
         print(csv_path)
-        assert os.path.isfile(csv_path)
         self.DATA = np.genfromtxt(csv_path, delimiter = ',')
-        #self.DATA = self.DATA[:200]
         self.X = self.DATA[:, 0:-1]
         self.Y = self.DATA[:, -1]
-        #self.X = np.pi*normalize(self.X, axis = 0, norm = 'max')
         self.X = np.pi*self.X/255
         
         
@@ -33,9 +27,3 @@ class MyCSVDatasetReader(Dataset):
 
     def _get_labels(self):
         return self.Y
-
-if __name__ == '__main__':
-    #dataset = MyCSVDatasetReader('./app/digits.csv')
-    dataset = MyCSVDatasetReader('./datasets/mnist_179_1200.csv')
-    sample = dataset.__getitem__(int(sys.argv[1]))
-    print(sample)
---
+++ README.md
@@ -0,0 +1,12 @@
+# QNMF
+## Datasets
+### The Breast Cancer
+Images from https://medmnist.com/  
+Slice features from https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic
+### The Covid - 19
+Images from https://github.com/UCSD-AI4H/COVID-CT  
+Blood test result from https://www.medrxiv.org/content/10.1101/2020.04.02.20051136v1
+## QCNN
+It has been uploaded.
+## VQC
+It can be reffered to the Pennylane document: https://pennylane.ai/qml/demos/tutorial_variational_classifier
